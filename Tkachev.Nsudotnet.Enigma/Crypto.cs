4a3fa6850e23aeecef4717dab1f7c86acd5ca051
Tkachov <Tkachov@TLab>
2016-04-02 22:47:21 +0600

    Enigma
    meh

---
+++ Tkachev.Nsudotnet.Enigma/Crypto.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Tkachev.Nsudotnet.Enigma {
+	class Crypto {
+		public static void encrypt(SymmetricAlgorithm crypto, string input_filename, string output_filename) { //key is in "output_filename.key.txt"
+			try {
+				byte[] original = File.ReadAllBytes(input_filename);				
+				crypto.GenerateKey();
+				crypto.GenerateIV();
+
+				byte[] encrypted;
+				ICryptoTransform encryptor = crypto.CreateEncryptor();
+				using(MemoryStream msEncrypt = new MemoryStream()) {
+					using(CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write)) {
+						csEncrypt.Write(original, 0, original.Length);
+						csEncrypt.FlushFinalBlock();
+						encrypted = msEncrypt.ToArray();
+					}
+				}
+
+				using(FileStream fs = new FileStream(output_filename, FileMode.Create, FileAccess.Write)) {
+					fs.Write(encrypted, 0, encrypted.Length);
+				}
+
+				if(output_filename.Contains(".txt"))
+					output_filename = output_filename.Replace(".txt", ".key.txt");
+				else
+					output_filename = output_filename + ".key.txt";
+
+				using(StreamWriter writetext = new StreamWriter(output_filename)) {
+					writetext.WriteLine(Convert.ToBase64String(crypto.IV));
+					writetext.WriteLine(Convert.ToBase64String(crypto.Key));
+				}
+			} catch(Exception e) {
+				Console.WriteLine("Error: {0}", e.Message);
+			}
+		}
+
+		public static void decrypt(SymmetricAlgorithm crypto, string input_filename, string key_filename, string output_filename) {
+			try {
+				byte[] input = File.ReadAllBytes(input_filename);
+				String IV_base64 = null, key_base64 = null;
+				using(var streamReader = new StreamReader(key_filename)) {
+					IV_base64 = streamReader.ReadLine();
+					key_base64 = streamReader.ReadLine();
+				}
+
+				byte[] output;
+				crypto.Key = Convert.FromBase64String(key_base64);
+				crypto.IV = Convert.FromBase64String(IV_base64);
+
+				ICryptoTransform decryptor = crypto.CreateDecryptor();
+				using(MemoryStream msDecrypt = new MemoryStream(input)) {
+					using(CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read)) {
+
+						byte[] buffer = new byte[16*1024];
+						using(MemoryStream ms = new MemoryStream()) {
+							int read;
+							while((read = csDecrypt.Read(buffer, 0, buffer.Length)) > 0) {
+								ms.Write(buffer, 0, read);
+							}
+							output = ms.ToArray();
+						}
+					}
+				}
+
+				using(FileStream fs = new FileStream(output_filename, FileMode.Create, FileAccess.Write)) {
+					fs.Write(output, 0, output.Length);
+				}
+			} catch(Exception e) {
+				Console.WriteLine("Error: {0}", e.Message);
+			}
+		}
+	}
+}
---
+++ Tkachev.Nsudotnet.Enigma/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace Tkachev.Nsudotnet.Enigma {
+	class Program {
+		private static bool invalidUsage(string[] args, Dictionary<String, SymmetricAlgorithm> factoryForPoor) {
+			if(args.Length < 3) {
+				Console.WriteLine("Not enough arguments.");
+				return true;
+			}
+
+			if(args[0] != "encrypt" && args[0] != "decrypt") {
+				Console.WriteLine("Unknown mode passed.");
+				return true;
+			}
+
+			if(args[0] == "encrypt" && args.Length != 4) {
+				Console.WriteLine("Wrong arguments number for 'encrypt' mode.");
+				return true;
+			}
+
+			if(args[0] == "decrypt" && args.Length != 5) {
+				Console.WriteLine("Wrong arguments number for 'decrypt' mode.");
+				return true;
+			}
+
+			if(!factoryForPoor.ContainsKey(args[2])) {
+				Console.WriteLine("Unknown algorithm passed.");
+				return true;
+			}
+
+			return false;
+		}
+
+		static void Main(string[] args) {			
+			Dictionary<String, SymmetricAlgorithm> factoryForPoor = new Dictionary<String, SymmetricAlgorithm>();
+			factoryForPoor.Add("aes", new AesManaged());
+			factoryForPoor.Add("des", new DESCryptoServiceProvider());
+			factoryForPoor.Add("rc2", new RC2CryptoServiceProvider());
+			factoryForPoor.Add("rijndael", new RijndaelManaged());
+
+			if(invalidUsage(args, factoryForPoor)) { //prints why usage is invalid
+				Console.WriteLine();
+				Console.WriteLine("Usage:");
+				Console.WriteLine("\tenigma encrypt <input file> <algorithm> <output file>");
+				Console.WriteLine("\tenigma decrypt <input file> <algorithm> <key file> <output file>");
+				return;
+			}
+
+			if(args[0] == "encrypt") Crypto.encrypt(factoryForPoor[args[2]], args[1], args[3]);
+			else Crypto.decrypt(factoryForPoor[args[2]], args[1], args[3], args[4]);
+		}
+	}
+}
