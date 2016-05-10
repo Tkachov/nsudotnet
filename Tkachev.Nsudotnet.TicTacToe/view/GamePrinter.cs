5e2dfbc1fcea9905d88caccfea10e5e33f9237c2
Tkachov <Tkachov@TLab>
2016-05-10 17:40:56 +0600

    TicTacToe/printing
    Removed string arrays (#8).

--- Tkachev.Nsudotnet.TicTacToe/view/GamePrinter.cs
+++ Tkachev.Nsudotnet.TicTacToe/view/GamePrinter.cs
@@ -24,62 +24,52 @@ namespace Tkachev.Nsudotnet.TicTacToe.view {
 			//you'd probably want me dead for this
 			//but that's all for the art and UI
 
-			String separator = "+";
-			String cellSeparator = new String('-', Game.COLS) + '+';
+			string separator = "+";
+			string cellSeparator = new string('-', Game.COLS) + '+';
 			for(int i = 0; i<Game.COLS; ++i)
 				separator += cellSeparator;
 
-			String smallSeparator = '+' + new String('-', Game.COLS) + '+';
+			string smallSeparator = '+' + new string('-', Game.COLS) + '+';			
 
-			String[] fieldLines = new String[Game.ROWS*(1+Game.ROWS)+1];
-			String[] infoLines = new String[2*(Game.ROWS+3)+1]; //2 fields (header, top line, rows, bottom line) + one empty line in between
-
-			int fieldLinesIndex = 0;
-			for(int i = 0; i<Game.ROWS; ++i) {
-				fieldLines[fieldLinesIndex++] = separator.ToString();
-				for(int j = 0; j<Game.ROWS; ++j) {
-					fieldLines[fieldLinesIndex++] = GetLine(game, i, j);
-				}
-			}
-			fieldLines[fieldLinesIndex++] = separator.ToString();
-
-			int infoLinesIndex = 0;
-			infoLines[infoLinesIndex++] = "Total result:";
-			infoLines[infoLinesIndex++] = smallSeparator;
-			for(int j = 0; j<Game.ROWS; ++j) {
-				infoLines[infoLinesIndex++] = GetSmallLine(game, j);
-			}
-			infoLines[infoLinesIndex++] = smallSeparator;
-			infoLines[infoLinesIndex++] = "";
+			int l1 = Game.ROWS*(1+Game.ROWS)+1;
+			int l2 = 2*(Game.ROWS+3)+1;
 
 			int fieldIndex = game.CurrentField;
-			if(fieldIndex != Game.CAN_MAKE_MOVE_AT_ANY_CELL) {
-				infoLines[infoLinesIndex++] = "Current field:";
-				infoLines[infoLinesIndex++] = smallSeparator;
-				int fieldRow = fieldIndex/Game.COLS;
-				int fieldCol = fieldIndex%Game.COLS;
-				for(int j = 0; j<Game.ROWS; ++j) {
-					infoLines[infoLinesIndex++] = GetSmallLineOfField(game, fieldRow, fieldCol, j);
-				}
-				infoLines[infoLinesIndex++] = smallSeparator;
-			} else {
-				for(int j = 0; j<Game.ROWS+3; ++j)
-					infoLines[infoLinesIndex++] = "";
-			}
+			int fieldRow = fieldIndex/Game.COLS;
+			int fieldCol = fieldIndex%Game.COLS;
+			if (fieldIndex == Game.CAN_MAKE_MOVE_AT_ANY_CELL) l2 = 3 + Game.ROWS;
 
-			int l1 = fieldLines.Length;
-			int l2 = infoLines.Length;
 			int l = (l1<l2 ? l2 : l1);
-			int defaultFieldLinesLength = 13;
-			if(l1>0)
-				defaultFieldLinesLength = fieldLines[0].Length;
 			for(int i = 0; i<l; ++i) {
+				int fieldLineI = i/(Game.ROWS + 1), fieldLineJ = i%(Game.ROWS + 1);
+
+				string fieldLine;
+				fieldLine = fieldLineJ == 0 ? separator : GetLine(game, fieldLineI, fieldLineJ-1);
+
+				string infoLine;
+				if (i == 0)
+					infoLine = "Total result:";
+				else if(i == 1 || i == Game.ROWS+2)
+					infoLine = smallSeparator;
+				else if (i < Game.ROWS + 2)
+					infoLine = GetSmallLine(game, i-2);
+				else if (i < l2) {
+					if (i == Game.ROWS + 3)
+						infoLine = "";
+					else if (i == Game.ROWS + 4)
+						infoLine = "Current field:";
+					else if (i == Game.ROWS + 5 || i == 2*Game.ROWS + 6)
+						infoLine = smallSeparator;
+					else 
+						infoLine = GetSmallLineOfField(game, fieldRow, fieldCol, i - Game.ROWS - 6);					
+				} else infoLine = "";
+
 				if(i<l1 && i<l2)
-					Console.WriteLine(fieldLines[i]+"     "+infoLines[i]);
+					Console.WriteLine(fieldLine+"     "+infoLine);
 				else if(i<l1)
-					Console.WriteLine(fieldLines[i]);
+					Console.WriteLine(fieldLine);
 				else
-					Console.WriteLine(new String(' ', defaultFieldLinesLength)+"     "+infoLines[i]);
+					Console.WriteLine(new string(' ', separator.Length)+"     "+infoLine);
 			}
 		}
 
